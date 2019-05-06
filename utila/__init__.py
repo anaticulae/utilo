b23a99c697b9da9f3b8e1c2b261ed0ace1afbeb4
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-05-06 09:15:35 +0200

    style(header): fix spelling error in copyright header

--- utila/__init__.py
+++ utila/__init__.py
@@ -2,7 +2,7 @@
 # C O P Y R I G H T
 #------------------------------------------------------------------------------
 # Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
-# Tis file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
