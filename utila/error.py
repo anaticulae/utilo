9d24731fce7aaf55081f043072f25140e3c27eb6
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-05-10 13:44:35 +0200

    feat(error): add option to use return value of saveme - or exit instantly

--- utila/error.py
+++ utila/error.py
@@ -36,24 +36,32 @@ def handle_error(*exceptions, code=1):
 CANCELLED_BY_USER = 130
 
 
-def saveme(function):
+def saveme(systemexit=True):
     """Protect against KeyboardInterrupt and beautify Exceptions
 
     Args:
-        function(callable): function which is invoked savely
+        systemexit(bool): return exit value of quit with SystemExit
+        user_function(callable): function which is invoked savely
     Returns:
         function-wrapper
     """
+    def decorating_function(user_function):
 
-    def wrapper():
-        try:
-            exit(function())
-        except KeyboardInterrupt:
-            logging('\nOperation cancelled by user')
-            exit(CANCELLED_BY_USER)
-        except Exception as error:  # pylint: disable=broad-except
-            logging_error(error)
-            logging_stacktrace()
-        exit(FAILURE)
+        def wrapper(*args, **kwds):
+            ret = None
+            try:
+                ret = user_function(*args, **kwds)
+            except KeyboardInterrupt:
+                logging('\nOperation cancelled by user')
+                ret = CANCELLED_BY_USER
+            except Exception as error:  # pylint: disable=broad-except
+                logging_error(error)
+                logging_stacktrace()
+                ret = FAILURE
+            if systemexit:
+                exit(ret)
+            return ret
 
-    return wrapper
+        return wrapper
+
+    return decorating_function
