48864e77552befc5690de92c285cd860b607468b
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-03-25 08:12:04 +0100

    refactor(move): move feature code to processor

--- utila/feature/__init__.py
+++ utila/feature/__init__.py
@@ -38,12 +38,17 @@ import utila
 import utila.cli
 import utila.feature.collector
 import utila.feature.description
+import utila.feature.processor
 import utila.utils
 
 WorkStep = collections.namedtuple('WorkStep', 'name inputs outputs')
 WorkSteps = typing.List[WorkStep]
 
-ErrorHook = typing.Tuple[Exception, str]
+Name = str
+CommandLineInterface = typing.List[utila.Command]
+Worker = callable  #pylint:disable=C0103
+Feature = typing.Tuple[Name, CommandLineInterface, Worker]
+Features = typing.List[Feature]
 
 
 class InterfaceMismatch(TypeError):
@@ -53,7 +58,7 @@ class InterfaceMismatch(TypeError):
 @dataclasses.dataclass
 class FeaturePackConfig:
     description: str = None
-    errorhook: ErrorHook = None
+    errorhook: 'utila.feature.processor.ErrorHook' = None
     failfastflag: bool = True
     flags: list = dataclasses.field(default_factory=list)
     multiprocessed: bool = False
@@ -155,7 +160,7 @@ def featurepack(  # pylint:disable=too-many-locals
     os.makedirs(outputpath, exist_ok=True)
 
     current_todo = determine_todo(args, config.flags)
-    completed = process(
+    completed = utila.feature.processor.process(
         workplan,
         config.name,
         errorhook=config.errorhook,
@@ -168,274 +173,8 @@ def featurepack(  # pylint:disable=too-many-locals
     return completed
 
 
-def process(
-        workplan: WorkSteps,
-        name: str = None,
-        todo: typing.List = None,
-        processes: int = 1,
-        pages: list = None,
-        errorhook: ErrorHook = None,
-        *,
-        failfast: bool = False,
-        profiling: bool = False,
-) -> int:
-    """Process the given features. The process ignores errors in
-    sub-steps and run till the end. If some error occurs, the process
-    returns an `FAILURE` after finishing. If the todo-list is empty,
-    every single step is processed.
-
-    Args:
-        workplan(WorkSteps): list of defined WorkStep's
-        name(str): name of executable
-        todo: list with steps to run, if no steps are None, every step is
-              executed.
-        processes(int): maximal parallel exection steps
-        pages(list): list with processed pages
-        errorhook(ErrorHook): if Error occurrs write it to ErrorHook
-        failfast(bool): quit after first failure
-        profiling(bool): if True, runtime of every single step is logged
-    Returns:
-        SUCCESS if all features process successfully, if not FAILURE
-    """
-    todo = prepare_process(todo, name, processes)
-
-    workplan = parallelize_workplan(
-        workplan,
-        root=name,
-        max_processes=processes,
-    )
-
-    executor = select_executor()
-    with executor(max_workers=processes) as pool:
-        failure = 0
-        for level in workplan:
-            # wait that level finishes without waiting, a next level which
-            # require resource of the current may will not find the
-            # resource, cause the excution is not done.
-            results = run_level(
-                level=level,
-                todo=todo,
-                pool=pool,
-                pages=pages,
-                profiling=profiling,
-            )
-
-            # write result
-            failure += write_level_result(
-                results,
-                errorhook=errorhook,
-                failfast=failfast,
-            )
-            if failfast and failure:
-                return utila.FAILURE
-    status = utila.FAILURE if failure else utila.SUCCESS
-    return status
-
-
-def run_level(level, todo, pool, pages, profiling):
-    results = []
-    for step in level:
-        # if todo is empty, nothing is selected, run every step
-        if step.name not in todo and todo:
-            utila.log(f'skipping: {step.name}')
-            continue
-        future = pool.submit(
-            callback,
-            step.inputs,
-            stepname=step.name,
-            output=step.outputs,
-            pages=pages,
-            profiling=profiling,
-        )
-        results.append(future)
-
-    return results
-
-
-def write_level_result(
-        results,
-        errorhook: ErrorHook = None,
-        *,
-        failfast=False,
-) -> int:
-    success = True
-    for result in results:
-        completed = result.result()
-        result, name, _ = completed
-        if isinstance(result, Exception):
-            if errorhook:
-                errorhook(result, name)
-            success = False
-            if failfast:
-                return utila.FAILURE
-        else:
-            written = write_result_safely(*completed)
-            if written == utila.FAILURE:
-                success = False
-                if failfast:
-                    return utila.FAILURE
-    return utila.SUCCESS if success else utila.FAILURE
-
-
-def callback(
-        hook: callable,
-        stepname: str,
-        output,
-        pages: list,
-        profiling: bool,
-) -> tuple:
-    """Run processing step.
-
-    Args:
-        hook(callable): function to execute
-        stepname(str): name of working step
-        output(str): path to write step output
-        pages(list): list of pages to processed
-        profiling(bool): if True log callback runtime
-    Returns:
-        Tuple of result, stepname and output
-    """
-    utila.log(f'processing: {stepname}')
-    # run runnable
-    runnable = functools.partial(
-        run_hook_safely,
-        hook=hook,
-        name=stepname,
-        stepoutput=output,
-        pages=pages,
-    )
-    try:
-        contextmanager = utila.profile if profiling else utila.nothing
-        with contextmanager(msg=stepname):
-            result = runnable()
-            utila.log(f'completed: {stepname}')
-    except Exception as exception:  # pylint:disable=broad-except
-        utila.error(f'failed: {stepname}')
-        result = exception  # pylint:disable=R0204
-    return [result, stepname, output]
-
-
-def run_hook_safely(
-        hook: callable,
-        name: str,
-        stepoutput,
-        pages,
-) -> int:
-    """Verify interface, run hook and catch Exception and log problem if
-    required.
-    """
-    sig = inspect.signature(hook)
-    try:
-        if utila.PAGES_FLAG in sig.parameters:
-            # optional page numbers flag
-            result = hook(pages=pages)
-        else:
-            result = hook()
-    except Exception as msg:  # pylint: disable=broad-except
-        utila.log_stacktrace()
-        utila.error('while processing %s' % name)
-        utila.error(msg)
-        raise
-
-    if isinstance(result, str):
-        result = [result]
-    # Verify result
-    variable_returnvalues = variable_parameter(stepoutput)
-    if result and len(stepoutput) != len(result) and not variable_returnvalues:
-        utila.error(f'wrong return value count in step: `{name}`')
-        utila.error(f'interface count: {len(stepoutput)}')
-        utila.error(f'return count from step: {len(result)}')
-        raise InterfaceMismatch
-    return result
-
-
-def variable_parameter(items: list) -> bool:
-    """Check if some path contains *-pattern to replace."""
-    result = any(['*' in item for item in items])
-    return result
-
-
-def write_result_safely(
-        result: typing.List[str],
-        processstep: str,
-        outputstep: typing.List[str],
-) -> int:
-    """Write `result`s to desired `outputstep`s and catch problems.
-
-    Args:
-        result(list): list of content to write
-        processstep(name): name of process step
-        outputstep(list): list of output paths
-    Returns:
-        Returns return code SUCCESS or FAILURE.
-    """
-    utila.call('write results')
-    try:
-        variable_returnvalues = variable_parameter(outputstep)
-        if variable_returnvalues:
-            assert len(outputstep) == 1, (f'only one variable parameter '
-                                          f'is supported {outputstep}')
-            outputstep = outputstep[0]
-            # Create parent folder if required:
-            # cli_example__multistep_pages/view_*.html
-            # adding list of files in parent folder is possible.
-            parent, _ = os.path.split(outputstep)
-            os.makedirs(parent, exist_ok=True)
-            # replace star-pattern to archive indexed output paths
-            outputstep = [
-                outputstep.replace('*', f'{index}')
-                for index, content in enumerate(result)
-            ]
-        for path, content in zip(outputstep, result):
-            # Ensure that parent folder exists. It is possible to create
-            # folder via `hello/folder/content.txt`.
-            parent, _ = os.path.split(path)
-            os.makedirs(parent, exist_ok=True)
-            utila.info('write %s' % path)
-            # write content to file.
-            utila.file_replace(path, content)
-        return utila.SUCCESS
-    except TypeError as msg:
-        utila.error(f'while processing {processstep}')
-        utila.error('wrong return value')
-        utila.error(f'current return value: {result}')
-        utila.error(msg)
-        return utila.FAILURE
-
-
-def select_executor():
-    # TODO: how to use multiprocessing with pytest, see pytest: 38.3.1
-    testrun = os.environ.get('PYTEST_PLUGINS', False)
-    executor = concurrent.futures.ProcessPoolExecutor
-    if testrun:
-        executor = concurrent.futures.ThreadPoolExecutor
-    return executor
-
-
-def prepare_process(todo, name, processes):
-    if todo is None:
-        todo = set()
-    # make todo unique
-    todo = set(todo)
-    # process all features, see some lines below
-    if 'all' in todo:
-        todo = set()
-    # log start of executable
-    utila.log(name)
-    if processes > 1:
-        utila.log('use multiple processes')
-    utila.log()
-    return todo
-
-
-Name = str
-CommandLineInterface = typing.List[utila.Command]
-Worker = callable  #pylint:disable=C0103
-Feature = typing.Tuple[Name, CommandLineInterface, Worker]
-
-
 def commandline(
-        features: typing.List[Feature],
+        features: Features,
         workplan: list,
 ) -> typing.List[utila.Command]:
     """Build command line interface due iterating searched features
@@ -838,6 +577,12 @@ def verify_interface(inputs, outputs, worker):
                     f'interface error {return_parameter} != {outputs}')
         return utila.FAILURE
     return utila.SUCCESS
+
+
+def variable_parameter(items: list) -> bool:
+    """Check if some path contains *-pattern to replace."""
+    result = any(['*' in item for item in items])
+    return result
 
 
 def determine_todo(args: dict, flags: list) -> typing.List[str]:
