28f30516351a8e432804d75021258d3cd6926867
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-02-04 10:16:54 +0100

    fix(group): return clean reference to avoid side effects

--- utila/group.py
+++ utila/group.py
@@ -34,7 +34,7 @@ def groupby_neighbors(items: list) -> list:
     [[1, 2, 3], [5, 6, 7]]
     """
     if not items:
-        return items
+        return []
     result = []
     collected = []
     for item in items:
