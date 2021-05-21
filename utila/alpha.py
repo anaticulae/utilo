7623a4068ca7b3e1b580dd7c8843a141d01fbc59
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-05-21 17:51:05 +0200

    format(alpha): improve readability

--- utila/alpha.py
+++ utila/alpha.py
@@ -69,13 +69,67 @@ def alphabetically(item: str) -> str:
     return replace(item).lower()
 
 
-TABLE = [
-    'α a', 'β b', 'γ g', 'δ d', 'ε ep', 'ζ z', 'η et', 'θ t', 'ι i', 'κ k',
-    'λ l', 'μ m', 'ν n', 'ξ x', 'π pi', 'ρ r', 'σ s', 'τ t', 'υ up', 'φ phi',
-    'χ c', 'ψ psi', 'ω o', 'Γ G', 'Δ E', 'Θ T', 'Λ L', 'Ξ X', 'Π PI', 'Σ SI',
-    'Υ UP', 'Φ PH', 'Ψ PS', 'Ω OM', '⁰ 0', '¹ 1', '² 2', '³ 3', '⁴ 4', '⁵ 5',
-    '⁶ 6', '⁷ 7', '⁸ 8', '⁹ 9', '₀ 0', '₁ 1', '₂ 2', '₃ 3', '₄ 4', '₅ 5', '₆ 6',
-    '₇ 7', '₈ 8', '₉ 9', 'ä ae', 'ö oe', 'ü ue', 'Ä AE', 'Ö OE', 'Ü UE'
-]
+TABLE = """\
+α           a
+β           b
+γ           g
+δ           d
+ε           ep
+ζ           z
+η           et
+θ           t
+ι           i
+κ           k
+λ           l
+μ           m
+ν           n
+ξ           x
+π           pi
+ρ           r
+σ           s
+τ           t
+υ           up
+φ           phi
+χ           c
+ψ           psi
+ω           o
+Γ           G
+Δ           E
+Θ           T
+Λ           L
+Ξ           X
+Π           PI
+Σ           SI
+Υ           UP
+Φ           PH
+Ψ           PS
+Ω           OM
+⁰           0
+¹           1
+²           2
+³           3
+⁴           4
+⁵           5
+⁶           6
+⁷           7
+⁸           8
+⁹           9
+₀           0
+₁           1
+₂           2
+₃           3
+₄           4
+₅           5
+₆           6
+₇           7
+₈           8
+₉           9
+ä           ae
+ö           oe
+ü           ue
+Ä           AE
+Ö           OE
+Ü           UE
+"""
 # TODO: REPLACE WITH SHARK OPERATOR, PYTHON 3.8
-MATCHED = {item.split()[0]: item.split()[1] for item in TABLE}
+MATCHED = {item.split()[0]: item.split()[1] for item in TABLE.strip().splitlines()}
