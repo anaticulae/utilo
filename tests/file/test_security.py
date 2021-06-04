19c8a851107b0c37489cf60f01ef80d089110b2a
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-06-04 16:47:10 +0200

    test(security): encrypt and copy content

--- tests/file/test_security.py
+++ tests/file/test_security.py
@@ -33,3 +33,19 @@ def test_file_create_private_binary(testdir):
     path = os.path.join(testdir.tmpdir, 'helm.txt')
     utila.file_create_binary(path, content, private=True)
     assert utila.file_read_binary(path) == content
+
+
+def test_copy_content_private(testdir):
+    testdir.mkdir('open')
+    testdir.mkdir('second')
+
+    utila.file_create('open/public.txt', utila.file_read(__file__))
+    binary_content = b'\xAA& Binary Content &\n'
+    utila.file_create_binary('second/binary.txt', binary_content)
+
+    utila.copy_content('.', 'private', recursive=True, private=True)
+
+    public = utila.file_read('private/open/public.txt')
+    assert public == utila.file_read(__file__)
+    binary = utila.file_read_binary('private/second/binary.txt')
+    assert binary == binary_content
