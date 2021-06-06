37d271e16a09c03b5f14c21c57f15f581235f6fa
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-06-06 17:20:02 +0200

    feat(secret): replace with utilasafe code

--- utila/secret.py
+++ utila/secret.py
@@ -7,24 +7,16 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import math
 import os
 
 import utila
 import utila.logger
 
 try:
-    # TODO: DO NOT RELASE BEFORE UPGRADE
-    # TODO: REPLACE WITH UPTODATE LIB
-    from Crypto.Cipher import AES
+    import utilasafe
 except ModuleNotFoundError:
-    utila.logger.error('could not use Crypto')
-    AES = None
-
-BLOCKSIZE = 32
-# TODO: REPLACE WITH WORKING ONE
-
-# UTILA_USER_PASSWORD
+    utila.logger.error('could not use utilasafe')
+    utilasafe = None  # pylint:disable=C0103
 
 
 def encrypt(plaintext: bytes) -> bytes:
@@ -34,9 +26,8 @@ def encrypt(plaintext: bytes) -> bytes:
             encoding='utf8',
             errors='xmlcharrefreplace',
         )
-    if AES:
-        plaintext = create_block(plaintext)
-        ciphertext = aes().encrypt(plaintext)
+    if utilasafe:
+        ciphertext = utilasafe.encrypt_password(plaintext)
     else:
         ciphertext = _encrypt_toy(plaintext)
     return ciphertext
@@ -44,49 +35,13 @@ def encrypt(plaintext: bytes) -> bytes:
 
 def decrypt(ciphertext: bytes, string: bool = False) -> bytes:
     """Convert cipher text to plain text."""
-    if AES:
-        plaintext = aes().decrypt(ciphertext)
-        plaintext = remove_block(plaintext)
+    if utilasafe:
+        plaintext = utilasafe.decrypt_password(ciphertext)
     else:
         plaintext = _decrypt_toy(ciphertext)
     if string:
         plaintext: str = plaintext.decode(encoding='utf8')
     return plaintext
-
-
-def password() -> str:
-    result = os.environ.get('UTILA_USER_PASSWORD', 'NOPASSWORD')
-    return result
-
-
-def aes():
-    # normalize password
-    pwd = password()
-    pwd = utila.secure_hash(pwd, digits=32)
-    pwd: bytes = pwd.encode('utf8')
-    # TODO: VERIFY PROS AND CONS OF MODE
-    result = AES.new(pwd, AES.MODE_ECB)
-    return result
-
-
-def create_block(content: bytes):
-    """\
-    >>> create_block(b'abc' * 15)
-    b'abcabcabcabcabcabcabcabcabcabcabcabcabcabcabc                   '
-    """
-    if len(content) % BLOCKSIZE == 0:
-        return content
-    missing = math.ceil(len(content) / BLOCKSIZE) * BLOCKSIZE - len(content)
-    content = content + b' ' * missing
-    return content
-
-
-def remove_block(content: bytes):
-    stripped = content.rstrip()
-    if content[len(stripped):len(stripped) + 1] == b'\n':
-        # valid file ends with newline
-        stripped = content[0:len(stripped) + 1]
-    return stripped
 
 
 def _encrypt_toy(plaintext: bytes) -> bytes:
@@ -107,3 +62,8 @@ def _decrypt_toy(ciphertext: bytes) -> bytes:
     plaintext = ([(item - key) % 256 for item in ciphertext])
     plaintext = bytes(plaintext)
     return plaintext
+
+
+def password() -> str:
+    result = os.environ.get('UTILASAFE_USER_PASSWORD', 'NOPASSWORD')
+    return result
