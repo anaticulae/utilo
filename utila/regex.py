17485e715feed61d19f882b6896fe96a2b665bec
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-25 19:13:04 +0100

    feat(regex): add flags option to compile regex

--- utila/regex.py
+++ utila/regex.py
@@ -18,33 +18,33 @@ def extract_match(matched) -> str:
     return matched.string[matched.span()[0]:matched.span()[1]]
 
 
-def match(pattern: str, content: str):
+def match(pattern: str, content: str, flags=None):
     r"""\
     >>> match(r'Abc\:', 'abc:')
     <re.Match object; span=(0, 4), match='abc:'>
     """
-    return re.match(pattern, content.strip(), flags=NOCASE_VERBOSE)
+    return re.match(pattern, content.strip(), flags=flags or NOCASE_VERBOSE)
 
 
-def search(pattern: str, content: str):
+def search(pattern: str, content: str, flags=None):
     r"""\
     >>> search(r'Abc\:', 'abc: ende')
     <re.Match object; span=(0, 4), match='abc:'>
     """
     if not isinstance(pattern, str):
         return pattern.search(content.strip())
-    return re.search(pattern, content.strip(), flags=NOCASE_VERBOSE)
+    return re.search(pattern, content.strip(), flags=flags or NOCASE_VERBOSE)
 
 
-def compiles(pattern: str):
+def compiles(pattern: str, flags=None):
     r"""\
     >>> compiles(r'\d+10 HELLO')
     re.compile('\\d+10 HELLO', re.IGNORECASE|re.VERBOSE)
     """
-    return re.compile(pattern, flags=NOCASE_VERBOSE)
+    return re.compile(pattern, flags=flags or NOCASE_VERBOSE)
 
 
-def finditer(pattern: str, text: str):
+def finditer(pattern: str, text: str, flags=None):
     r"""\
     >>> [extract_match(item) for item in finditer(pattern=r'\d+', text=r'   10 HELLO 38 19')]
     ['10', '38', '19']
@@ -53,5 +53,5 @@ def finditer(pattern: str, text: str):
     """
     if not isinstance(pattern, str):
         return pattern.finditer(text)
-    result = re.finditer(pattern, text, flags=NOCASE_VERBOSE)
+    result = re.finditer(pattern, text, flags=flags or NOCASE_VERBOSE)
     return result
