df117d920ca51aa65ad4dfa84a5bd398d05a0139
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-02-26 12:09:34 +0100

    test(move): move tests to separate module

--- tests/classifier/test_classifier.py
+++ tests/classifier/test_classifier.py
@@ -11,27 +11,22 @@ import utila
 
 
 def test_classifier_same_area_cluster():
-
     todo = [
         ((0, 0, 50, 50), 'A'),
         ((200, 200, 300, 300), 'B'),
         ((0, 0, 50, 50), 'C'),
         ((250, 250, 300, 300), 'D'),
     ]
-
     clusters = utila.same_area_cluster(todo, max_difference=0.0)
     clusters = [item[:] for item in clusters]
-
     expected = [[
         ((0, 0, 50, 50), 'A'),
         ((0, 0, 50, 50), 'C'),
     ]]
-
     assert clusters == expected
 
 
 def test_classifier_three_side_equal():
-
     todo = [
         ((0, 0, 50, 50), 'A'),
         ((0, 0, 100, 50), 'B'),
@@ -39,18 +34,14 @@ def test_classifier_three_side_equal():
         ((0, 0, 200, 50), 'D'),
         ((200, 200, 250, 250), 'E'),
     ]
-
     clusters = utila.three_side_equal_cluster(todo)
-
     expected = [[
         ((0, 0, 50, 50), 'A'),
         ((0, 0, 100, 50), 'B'),
         ((0, 0, 25, 50), 'C'),
         ((0, 0, 200, 50), 'D'),
     ]]
-
     assert len(clusters) == len(expected), clusters
-
     sorted_result = sorted(clusters[0], key=lambda item: item[1])
     assert sorted_result == expected[0], sorted_result
 
