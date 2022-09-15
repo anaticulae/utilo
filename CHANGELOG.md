e96a508fbf9fe1d15210d1c790b8a72b1aa97712
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-09-15 23:05:14 +0200

    2.86.1 auto generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v2.86.1
+
 ## v2.86.0
 
 ### Feature
--- utila/__init__.py
+++ utila/__init__.py
@@ -7,7 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-__version__ = '2.86.0'
+__version__ = '2.86.1'
 
 import os
 
