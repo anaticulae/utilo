d98b253f7cbe686598e91411b12ccad7b7abe789
Automated Release <automated_release@ostia.la>
2022-12-03 15:47:03 +0000

    2.96.1 auto generated release
    >>> Fix
    
    * use backup if no git repository is given (aa6101952a63)

--- utila/__init__.py
+++ utila/__init__.py
@@ -7,7 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-__version__ = '2.96.0'
+__version__ = '2.96.1'
 
 import os
 
