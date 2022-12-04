893fc3c242465b233b258ccd032756d72984e1d6
Automated Release <automated_release@ostia.la>
2022-12-04 21:51:34 +0000

    2.97.0 auto generated release
    >>> Feature
    
    * add parameter to disable package data inclusion (7fdce541d6b0)
    
    >>> Fix
    
    * add missing install requirements (c6b9133c9b12)

--- utila/__init__.py
+++ utila/__init__.py
@@ -7,7 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-__version__ = '2.96.1'
+__version__ = '2.97.0'
 
 import os
 
