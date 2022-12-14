f9f95a703d470f132e6970352f4d9affc1d6b512
Automated Release <automated_release@ostia.la>
2022-12-14 13:00:25 +0000

    2.98.0 auto generated release
    >>> Feature
    
    * ensure that packages is defined correctly (299f96ae6ee0)

--- utila/__init__.py
+++ utila/__init__.py
@@ -7,7 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-__version__ = '2.97.0'
+__version__ = '2.98.0'
 
 import os
 
