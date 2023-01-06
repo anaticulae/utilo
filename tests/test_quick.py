1fe55bd9dc2e7ef456a1e5e637a0938a7931994a
Helmut Konrad Schewe <helmutus@outlook.com>
2023-01-06 23:20:40 +0000

    test(quick): verify quick install

---
+++ tests/test_quick.py
@@ -0,0 +1,30 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2023 by Helmut Konrad Schewe. All rights reserved.
+# This file is property of Helmut Konrad Schewe. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import setuptools._distutils.core
+
+import utila
+
+
+def test_quick_install(td, mp):
+    root = str(td.tmpdir)
+    project = 'no_project'
+    cmd = f'baw init {project} "nice example"'
+    utila.run(
+        cmd,
+        cwd=root,
+    )
+    setup = td.tmpdir.join('setup.py')
+
+    def no_install(**attrs):  # pylint:disable=W0613
+        pass
+
+    with mp.context() as context:
+        context.setattr(setuptools._distutils.core, 'setup', no_install)  # pylint:disable=W0212
+        utila.install(root=setup)
