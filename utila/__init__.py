88e161a5816bd7e9a6f7042c3068edb0cd17846a
Automated Release <automated_release@ostia.la>
2023-01-09 15:15:58 +0000

    2.100.1 auto generated release
    >>> Fix
    
    * describe requires annotated commits (f916875f81db)

--- utila/__init__.py
+++ utila/__init__.py
@@ -7,7 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-__version__ = '2.100.0'
+__version__ = '2.100.1'
 
 import os
 
