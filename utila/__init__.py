057e245b6e0011829ec94d5e30cea2e9b6c9e645
Automated Release <automated_release@ostia.la>
2023-01-13 21:20:40 +0000

    2.101.1 auto generated release

--- utila/__init__.py
+++ utila/__init__.py
@@ -7,7 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-__version__ = '2.101.0'
+__version__ = '2.101.1'
 
 import os
 
