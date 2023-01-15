f38ec206894d300bad804d855ae3574d9327663b
Automated Release <automated_release@ostia.la>
2023-01-15 18:05:49 +0000

    2.102.0 auto generated release
    >>> Feature
    
    * make interface more robust (8778db7c869b)
    * add method to determine cwd (3e6c38ed03ed)
    * return success (aeee467b9993)

--- utila/__init__.py
+++ utila/__init__.py
@@ -7,7 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-__version__ = '2.101.1'
+__version__ = '2.102.0'
 
 import os
 
