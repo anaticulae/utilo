8eb73533d7e53ab3749a22c32907d165834a7c36
Automated Release <automated_release@ostia.la>
2023-01-17 00:15:29 +0000

    2.103.0 auto generated release
    >>> Feature
    
    * make message more verbose (f49a3f99fd00)
    * make string checker more flexible (ba8f42dd94e1)
    
    >>> Fix
    
    * remove unreachable code (c7f1115596b7)

--- utila/__init__.py
+++ utila/__init__.py
@@ -7,7 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-__version__ = '2.102.0'
+__version__ = '2.103.0'
 
 import os
 
