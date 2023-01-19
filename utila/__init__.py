8ccc5f6e8b5a5e93a31ce4550408bd48e15fb6b6
Automated Release <automated_release@ostia.la>
2023-01-19 11:20:48 +0000

    2.103.1 auto generated release
    >>> Fix
    
    * we want to have the g, see baw tool (24dd4abe91ab)

--- utila/__init__.py
+++ utila/__init__.py
@@ -7,7 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-__version__ = '2.103.0'
+__version__ = '2.103.1'
 
 import os
 
