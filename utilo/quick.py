57770c3a8b292776cbe8932b82fb7116afab5222
Helmut Konrad Schewe <helmutus@outlook.com>
2026-03-04 02:52:18 +0100

    chore(quick): extend python labels

--- utilo/quick.py
+++ utilo/quick.py
@@ -20,6 +20,10 @@ AUTHOR_EMAIL = 'helmutus@outlook.com'
 CLASSIFIERS = [
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
+    'Programming Language :: Python :: 3.14',
 ]
 
 
