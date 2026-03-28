2d6d658e4234ab469ab7d6e553daf356e077380b
Helmut Konrad Schewe <helmutus@outlook.com>
2026-03-28 13:53:13 +0100

    chore(release): 2.106.3

--- utilo/__init__.py
+++ utilo/__init__.py
@@ -7,7 +7,7 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-__version__ = '2.106.2'
+__version__ = '2.106.3'
 
 import os
 
